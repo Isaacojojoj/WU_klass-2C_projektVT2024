e5deed13ac6663aa405b254532a85ec17c60c8a9
Isaacojojoj <Isaac.ojo@elev.ga.ntig.se>
2024-05-20 14:40:50 +0200

    ä
    ä

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-const burger = document.getElementById('burger')
-const linkList = document.getElementById('link_list')
+// bildspel
 
+const arrowLeft = document.getElementById('arrow_left')
+const arrowRight = document.getElementById('arrow_right')
 
-function toggleMenu() {
+var imgGalleryImgSources = ["img/bildspel/bildspel_1.jpg", "img/bildspel/bildspel_2.jpg", "img/bildspel/bildspel_3.jpg", "img/bildspel/bildspel_4.jpg", "img/bildspel/bildspel_5.jpg", "img/bildspel/bildspel_6.jpg"]
+var imgGalleryIndex = 0
 
-    document.getElementById('link_list').classList.toggle('links_visible')
-    document.getElementById('burger').classList.toggle('burger_in_x_form')
+function newImageRight() {
+    if (imgGalleryIndex == imgGalleryImgSources.length - 1) {imgGalleryIndex = 0}
+    else {imgGalleryIndex = imgGalleryIndex + 1}
+    document.getElementById('gallery_img').src = imgGalleryImgSources[imgGalleryIndex]
 }
 
-document.getElementById('burger').addEventListener('click',toggleMenu)
-
-
-
-// hamburgermeny
-function toggleMenu() {
-    linkList.classList.toggle('links_visible')
-    burger.classList.toggle('burger_in_x_form')
+function newImageLeft() {
+    if (imgGalleryIndex == 0) {imgGalleryIndex = imgGalleryImgSources.length - 1}
+    else {imgGalleryIndex = imgGalleryIndex - 1}
+    document.getElementById('gallery_img').src = imgGalleryImgSources[imgGalleryIndex]
 }
 
-burger.addEventListener('click', toggleMenu)
-
+arrowLeft.addEventListener('click', newImageLeft)
+arrowRight.addEventListener('click', newImageRight)

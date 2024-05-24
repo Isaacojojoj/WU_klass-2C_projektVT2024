238e3a8fb0c5120d33aabc97f8f5b324a335bac8
Isaacojojoj <Isaac.ojo@elev.ga.ntig.se>
2024-05-24 11:17:04 +0200

    hehe
    hehe

--- js/script.js
+++ js/script.js
@@ -1,22 +1,10 @@
-// bildspel
+// hamburgermeny
+const burger = document.getElementById('burger')
+const linkList = document.getElementById('link_list')
 
-const arrowLeft = document.getElementById('arrow_left')
-const arrowRight = document.getElementById('arrow_right')
-
-var imgGalleryImgSources = ["img/bildspel/bildspel_1.jpg", "img/bildspel/bildspel_2.jpg", "img/bildspel/bildspel_3.jpg", "img/bildspel/bildspel_4.jpg", "img/bildspel/bildspel_5.jpg", "img/bildspel/bildspel_6.jpg"]
-var imgGalleryIndex = 0
-
-function newImageRight() {
-    if (imgGalleryIndex == imgGalleryImgSources.length - 1) {imgGalleryIndex = 0}
-    else {imgGalleryIndex = imgGalleryIndex + 1}
-    document.getElementById('gallery_img').src = imgGalleryImgSources[imgGalleryIndex]
+function toggleMenu() {
+    linkList.classList.toggle('links_visible')
+    burger.classList.toggle('burger_in_x_form')
 }
 
-function newImageLeft() {
-    if (imgGalleryIndex == 0) {imgGalleryIndex = imgGalleryImgSources.length - 1}
-    else {imgGalleryIndex = imgGalleryIndex - 1}
-    document.getElementById('gallery_img').src = imgGalleryImgSources[imgGalleryIndex]
-}
-
-arrowLeft.addEventListener('click', newImageLeft)
-arrowRight.addEventListener('click', newImageRight)
+burger.addEventListener('click', toggleMenu)
